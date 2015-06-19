586482b2bea2bce2787ed8c9d4366f674da2dd61
Ennio Simoes <ennio.simoes@outlook.com>
2015-06-19 20:32:00 -0300

    Add concat CSS e imagemin .jpg e .png

--- css.php
+++ css.php
@@ -1,10 +1,13 @@
-<link rel="stylesheet" href="assets/css/bootstrap.min.css">  
-<link rel="stylesheet" href="assets/css/font-awesome.min.css">  
-<link rel="stylesheet" href="assets/css/animate.min.css">  
-<link rel="stylesheet" href="assets/css/responsive.css">
-
 <!--Sliders e banners-->
 <link rel="stylesheet" type="text/css" href="assets/slick/slick.css"/>
 <!-- Add the new slick-theme.css if you want the default styling -->
 <link rel="stylesheet" type="text/css" href="assets/slick/slick-theme.css"/>
-<link rel="stylesheet" href="assets/css/main.css">
+
+
+<link rel="stylesheet" href="assets/css/all.css">  
+
+<!--<link rel="stylesheet" href="assets/css/bootstrap.min.css">  
+<link rel="stylesheet" href="assets/css/font-awesome.min.css">  
+<link rel="stylesheet" href="assets/css/animate.min.css">  
+<link rel="stylesheet" href="assets/css/responsive.css">
+<link rel="stylesheet" href="assets/css/main.css">-->
--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -3,6 +3,9 @@ var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
 var notify = require("gulp-notify");
 
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
 //Junta tudo em all.js
 gulp.task('scripts', function() {
   return gulp.src([
@@ -19,10 +22,39 @@ gulp.task('scripts', function() {
 
 //Cria CSS minificado
 gulp.task('minify-css', function() {
-  return gulp.src('./../assets/css/src/*.css')
+  return gulp.src('./../assets/css/sass/*.css') //Arquivos Originais
     .pipe(minifyCss({compatibility: 'ie8'}))
     .pipe(gulp.dest('./../assets/css'))
     .pipe( notify( 'CSS OK!' ));
+});
+
+//Junta tudo em all.css
+gulp.task('concat-css', function() {
+    return gulp.src([
+        './../assets/css/bootstrap.min.css',
+        './../assets/css/font-awesome.min.css',
+        './../assets/css/animate.min.css',
+        './../assets/css/responsive.css',
+        './../assets/css/main.css',
+    ])
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('./../assets/css/'))
+    .pipe( notify( 'CSS unificado com sucesso!' ));
+});
+
+gulp.task('imagemin', function () {
+    return gulp.src('./../assets/img/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./../assets/img'));
+});
+
+//Roda watch no css
+gulp.task( 'otimizar-site', function() {
+    gulp.run('scripts', 'minify-css', 'concat-css', 'imagemin');
 });
 
 //Roda watch no css
